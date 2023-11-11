467fb35413146c9cb5c4aa820c07bfa6190c0e97
Julius Omoleye <asyncdeveloper610@gmail.com>
2023-11-11 22:13:22 +0000

    add v2

--- python/pyOps.py
+++ python/pyOps.py
@@ -1,78 +1,70 @@
+import yaml
 import subprocess
 import os
-import jinja2
+from jinja2 import Environment, FileSystemLoader
 
-def generate_terraform_script(output_path):
-    # Sample Terraform template
-    terraform_template = """
-    provider "aws" {
-      region = "us-west-2"
-    }
+def load_yaml(file_path):
+    with open(file_path, 'r') as file:
+        return yaml.safe_load(file)
 
-    resource "aws_instance" "example" {
-      ami           = "ami-0c55b159cbfafe1f0"
-      instance_type = "t2.micro"
-    }
-    """
-    with open(output_path, 'w') as file:
-        file.write(terraform_template)
+def generate_terraform_files(data, template_dir='templates', output_dir='terraform'):
+    env = Environment(loader=FileSystemLoader(template_dir))
+    # Example: Generate Terraform files for S3
+    if 's3_buckets' in data:
+        template = env.get_template('s3.tf.j2')
+        rendered_content = template.render({'s3_buckets': data['s3_buckets']})
+        with open(os.path.join(output_dir, "s3.tf"), 'w') as f:
+            f.write(rendered_content)
+    # Add similar sections for other Terraform resources like VPC, EC2, etc.
+    print("Terraform files generated.")
 
-def generate_ansible_playbook(output_path):
-    # Sample Ansible playbook template
-    ansible_template = """
-    - hosts: all
-      tasks:
-      - name: Install nginx
-        apt:
-          name: nginx
-          state: present
-    """
-    with open(output_path, 'w') as file:
-        file.write(ansible_template)
+def apply_terraform(output_dir='terraform'):
+    subprocess.run(["terraform", "init"], cwd=output_dir)
+    subprocess.run(["terraform", "apply", "-auto-approve"], cwd=output_dir)
 
-def setup_kubernetes_objects():
-    # This could be replaced with dynamic manifest generation or Helm chart usage
-    k8s_manifest = """
-    apiVersion: apps/v1
-    kind: Deployment
-    metadata:
-      name: nginx-deployment
-    spec:
-      selector:
-        matchLabels:
-          app: nginx
-      replicas: 2
-      template:
-        metadata:
-          labels:
-            app: nginx
-        spec:
-          containers:
-          - name: nginx
-            image: nginx:1.14.2
-            ports:
-            - containerPort: 80
-    """
-    with open("k8s_manifest.yaml", 'w') as file:
-        file.write(k8s_manifest)
-    subprocess.run(["kubectl", "apply", "-f", "k8s_manifest.yaml"])
+def generate_ansible_playbooks(data, template_dir='templates', output_dir='ansible'):
+    env = Environment(loader=FileSystemLoader(template_dir))
+    template = env.get_template('playbook.yaml.j2')
+    for playbook in data.get('playbooks', []):
+        rendered_content = template.render(playbook)
+        playbook_path = os.path.join(output_dir, f"{playbook['name']}_playbook.yaml")
+        with open(playbook_path, 'w') as f:
+            f.write(rendered_content)
+    print("Ansible playbooks generated.")
+
+def run_ansible_playbooks(output_dir='ansible'):
+    for playbook in os.listdir(output_dir):
+        if playbook.endswith(".yaml") or playbook.endswith(".yml"):
+            subprocess.run(["ansible-playbook", os.path.join(output_dir, playbook)])
+
+def generate_kubernetes_files(data, template_dir='templates', output_dir='kubernetes'):
+    env = Environment(loader=FileSystemLoader(template_dir))
+    # Example: Generate Kubernetes deployment files
+    # Add similar sections for other Kubernetes resources like services, ingress, etc.
+    print("Kubernetes files generated.")
+
+def apply_kubernetes_configs(output_dir='kubernetes'):
+    for file in os.listdir(output_dir):
+        if file.endswith(".yaml") or file.endswith(".yml"):
+            subprocess.run(["kubectl", "apply", "-f", os.path.join(output_dir, file)])
 
 def main():
-    # Step 1: Generate and Apply Terraform Script
-    terraform_script_path = 'terraform/main.tf'
-    os.makedirs(os.path.dirname(terraform_script_path), exist_ok=True)
-    generate_terraform_script(terraform_script_path)
-    subprocess.run(["terraform", "init"], cwd=os.path.dirname(terraform_script_path))
-    subprocess.run(["terraform", "apply", "-auto-approve"], cwd=os.path.dirname(terraform_script_path))
+    data = load_yaml('infra_setup.yaml')
 
-    # Step 2: Generate and Run Ansible Playbook
-    ansible_playbook_path = 'ansible/playbook.yml'
-    os.makedirs(os.path.dirname(ansible_playbook_path), exist_ok=True)
-    generate_ansible_playbook(ansible_playbook_path)
-    subprocess.run(["ansible-playbook", ansible_playbook_path])
+    # Generate and Apply Terraform Scripts
+    os.makedirs('terraform', exist_ok=True)
+    generate_terraform_files(data.get('terraform', {}))
+    apply_terraform()
 
-    # Step 3: Setup Kubernetes Objects
-    setup_kubernetes_objects()
+    # Generate and Run Ansible Playbooks
+    os.makedirs('ansible', exist_ok=True)
+    generate_ansible_playbooks(data.get('ansible', {}))
+    run_ansible_playbooks()
+
+    # Generate and Apply Kubernetes Configurations
+    os.makedirs('kubernetes', exist_ok=True)
+    generate_kubernetes_files(data.get('kubernetes', {}))
+    apply_kubernetes_configs()
 
 if __name__ == "__main__":
     main()
