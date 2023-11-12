34046079bb3250da93ce4081f7ef062cbce6c597
Julius Omoleye <asyncdeveloper610@gmail.com>
2023-11-12 23:13:28 +0000

    deploy to pypi

--- PyOps/setup.py
+++ PyOps/setup.py
@@ -2,14 +2,24 @@ from setuptools import setup, find_packages
 
 setup(
     name='pythonOps',
-    version='0.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
-       "jinja2"
+       "jinja2",
+       "pyyaml"
     ],
     # Other metadata
     author='Julius Omoleye',
     author_email='julius.omoleye@outlook.com',
     description='A groundbreaking Python application that simplifies configuration management across multiple platforms, including Terraform, Ansible, and Kubernetes. Write one config file and let PyOps generate the rest!',
-    url='https://github.com/AsyncDeveloper245/10x.git'
+    url='https://github.com/AsyncDeveloper245/10x.git',
+    
+    entry_points={
+        'console_scripts': [
+            'pyops=PyOps.pyOps:main',
+        ],
+    },
+    
+    
+    
 )
